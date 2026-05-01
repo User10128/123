a155fc3782f55f4e599b237c10bd15571224897d
User10128 <willlokey3@gmail.com>
2026-04-30 21:03:53 -0400

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,19 @@
-const CACHE_NAME = 'checkers-v2';
-const OFFLINE_PAGE = 'offline.html';
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.add(OFFLINE_PAGE))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'offline.html'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.match(OFFLINE_PAGE);
-      })
-    );
-  }
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match('offline.html');
+    })
+  );
 });
