c0c3a2d45329837fe78c29e1ad85476deeb9db4d
User10128 <willlokey3@gmail.com>
2026-05-03 21:17:43 -0400

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,4 @@
-const CACHE_NAME = 'checkers-v1';
-// No slashes at the start helps with Cloudflare routing
-const ASSETS = [
-  'index.html',
-  'offline.html'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      // This forces the browser to fetch a fresh copy from the server to save
-      return Promise.all(
-        ASSETS.map(asset => cache.add(new Request(asset, {cache: 'reload'})))
-      );
-    })
-  );
-  self.skipWaiting();
-});
-
-// Bare-bones fetch listener in service-worker.js
+// This file can be mostly empty, but the 'fetch' event is required for installation
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      // If ANY request fails, try to return the offline page
-      return caches.match('offline.html');
-    })
-  );
+  // Satisfies the browser's requirement for a functional service worker
 });
