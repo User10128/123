0b67eff73a6d99415186c34ceefd902d99ed4067
User10128 <willlokey3@gmail.com>
2026-05-05 16:32:30 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,32 @@
+const CACHE_NAME = 'offline-cache-v1';
 
+// Add the URLs you want to cache here
+const urlsToCache = [
+    '/',
+    '/index.html'
+];
+
+// 1. Install Event: Opens the cache and adds the files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// 2. Fetch Event: Intercepts requests and serves from cache if available
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return the cached response if found
+                if (response) {
+                    return response;
+                }
+                // Otherwise, fetch it from the network
+                return fetch(event.request);
+            })
+    );
+});
